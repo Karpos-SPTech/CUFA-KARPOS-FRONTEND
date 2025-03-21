b3e3781be0c93e9239853bb6416f89964cfb7e59
LG-Carvalho <lgcarvalho2@stefanini.com>
2025-03-21 10:08:15 -0300

    Dev (#2)
    * Subindo variaveis de cor, tipografia e outras funcionalidades. Importando icones e imagens
    
    * Adicionando as imagens laterais
    
    * Subindo cards "Sobre Nós"
    
    * Finalizando a tela
    
    * icon cufa
    
    * Acerto de Alinhamentos e Redimensionamento de Imagens
    
    * Adicionando Vlibras ao Site
    
    ---------
    
    Co-authored-by: AlexandreFrizzon <alebenefri@gmail.com>

---
+++ src/app/script.js
@@ -0,0 +1,48 @@
+const sliderTrack = document.querySelector('.slider-track');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].offsetWidth;
+let isMoving = false;
+
+function moveLeft() {
+  if (isMoving) return;
+  isMoving = true;
+
+  const lastSlide = sliderTrack.lastElementChild;
+  sliderTrack.style.transition = 'none';
+  sliderTrack.insertBefore(lastSlide, sliderTrack.firstElementChild);
+
+  sliderTrack.style.transform = `translateX(-${slideWidth}px)`;
+  setTimeout(() => {
+    sliderTrack.style.transition = 'transform 0.5s ease';
+    sliderTrack.style.transform = `translateX(0)`;
+    setTimeout(() => (isMoving = false), 500);
+  }, 50);
+}
+
+function moveRight() {
+  if (isMoving) return;
+  isMoving = true;
+
+  const firstSlide = sliderTrack.firstElementChild;
+  sliderTrack.style.transition = 'transform 0.5s ease';
+  sliderTrack.style.transform = `translateX(-${slideWidth}px)`;
+
+  setTimeout(() => {
+    sliderTrack.style.transition = 'none';
+    sliderTrack.appendChild(firstSlide);
+    sliderTrack.style.transform = `translateX(0)`;
+    isMoving = false;
+  }, 500);
+}
+
+let autoplayInterval = setInterval(moveRight, 3000);
+
+const sliderContainer = document.querySelector('.slider-container');
+sliderContainer.addEventListener('mouseenter', () => {
+  clearInterval(autoplayInterval);
+});
+
+sliderContainer.addEventListener('mouseleave', () => {
+  autoplayInterval = setInterval(moveRight, 3000);
+});
+
